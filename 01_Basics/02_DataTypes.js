902f2befa82d998b8e8add005a2fbb901f806615
Harsh-150 <153707537+Harsh-150@users.noreply.github.com>
2023-12-13 07:56:46 +0000

    Data Types

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,14 @@
+let name = "Harsh"
+let age = 19
+let isLoggedIn = false
+let state = null
+let temp
+
+// null -> stand alone value(empty value)
+// undefined -> 
+// symbol -> unique
+
+// object
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
