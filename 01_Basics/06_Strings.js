67493914df73c50227ac980b04102a4326c91674
Harsh-150 <153707537+Harsh-150@users.noreply.github.com>
2023-12-18 13:28:03 +0000

    strings in JS

---
+++ 01_Basics/06_Strings.js
@@ -0,0 +1,31 @@
+const name = "harsh";
+const age = 19;
+
+console.log(name + age);// bad practice
+
+console.log(`my name is ${name} and i am ${age}.`);
+
+const getname = new String('harsh gupta');
+console.log(getname[0]);
+console.log(getname[1]);
+console.log(getname[2]);
+console.log(getname[4]);
+console.log(getname[6]);
+console.log(getname[7]);
+
+console.log(getname.length);
+console.log(getname.toUpperCase());
+console.log(getname.charAt(5));
+console.log(getname.indexOf('h'));
+
+const newString = getname.substring(1,5);//1 is inclusive and 5 is not inclusive
+console.log(newString);
+
+const newString2 = "    qwerxc  poiuyt   ";
+console.table([newString2, newString2.trim()]);
+
+
+const url = "https://harsh.com/harsh%20gupta";
+console.log(url.replace('%20','-'));
+console.log(url.includes('harsh'));
+console.log(url.includes('ayush'));
