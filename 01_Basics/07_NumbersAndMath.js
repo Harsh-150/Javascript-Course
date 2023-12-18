da3d970f35090a6d6c726bce3ce0f80d873f369e
Harsh-150 <153707537+Harsh-150@users.noreply.github.com>
2023-12-18 16:54:43 +0000

    Math & Num

---
+++ 01_Basics/07_NumbersAndMath.js
@@ -0,0 +1,23 @@
+const score = 100;
+console.log(score);
+
+const balance = new Number(400);
+console.log(balance);
+
+let s = balance.toString();
+console.log(s);
+console.log(s.length);
+
+console.log(balance.toFixed(3));
+
+const hundreds = 100000000000;
+console.log(hundreds.toLocaleString());
+
+
+
+console.log(Math.abs(-876));
+console.log(Math.round(78.98));
+console.log(Math.ceil(7.3));
+console.log(Math.floor(78.929));
+
+console.log(Math.random());
