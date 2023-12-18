e3299695e23268bb9744808d6186a14e1175cc7d
Harsh-150 <153707537+Harsh-150@users.noreply.github.com>
2023-12-18 04:43:55 +0000

    data types

---
+++ 01_Basics/05_DataTypes.js
@@ -0,0 +1,30 @@
+// Primitive and Non-Primitive Data Types
+
+/*primitive:
+    7 types:
+        string, number, boolean, null, undefined, symbol, BigInt
+*/
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(id === anotherId);
+
+
+const bigNumber = 123456789098765432n;// bigInt 
+
+
+/* non-primitive(reference type):
+    array, objects, functions
+*/
+const avatars = ["Ram", "Krishna", "Vaman"];
+
+let obj = {
+    name: "Harsh",
+    age: 19,
+};
+
+const func = function(){
+    console.log("Hello World")
+}
+
+console.log(typeof null);
