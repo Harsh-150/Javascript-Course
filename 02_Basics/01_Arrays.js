9466127a78f8c290e122bcb1f8192721d1b71538
Harsh-150 <153707537+Harsh-150@users.noreply.github.com>
2023-12-19 18:12:57 +0000

    arrays javascript

---
+++ 02_Basics/01_Arrays.js
@@ -0,0 +1,15 @@
+// Declaration method 1 for array
+const arr = [1,2,3,4,"harsh",true];
+
+// Declaration method 2 for array
+const arr2 = new Array(1,2,3,4,5,6,7,8);
+
+// arrray methods
+arr.push(6);
+console.log(arr);
+
+arr2.pop();
+console.log(arr2);
+
+arr.unshift(101);//element added at start
+console.log(arr);
