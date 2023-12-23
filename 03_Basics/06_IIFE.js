4515819f2f45b11b6a1c6b24c922192d3a9baec9
Harsh-150 <153707537+Harsh-150@users.noreply.github.com>
2023-12-23 08:21:21 +0000

    IIFE

---
+++ 03_Basics/06_IIFE.js
@@ -0,0 +1,11 @@
+// Immediately Invoked Function Expressions
+//()()
+
+(function hello(){
+    // named IIFE
+    console.log(`DB connected`)
+})(); //semicolon is necessary
+
+((name)=>{
+    console.log(`DB connected 2 ${name}`);
+})('Aakriti');
