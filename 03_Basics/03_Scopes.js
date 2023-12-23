31228bdc6676c8daac06f622ab720506529f7a1f
Harsh-150 <153707537+Harsh-150@users.noreply.github.com>
2023-12-23 06:29:11 +0000

    more in scopes

---
+++ 03_Basics/03_Scopes.js
@@ -0,0 +1,35 @@
+{}  // ----->  scope
+ let a = 10
+ if(true){
+    let a = 20
+    let b = 30
+
+    console.log("inside function: ", a);
+ }
+ console.log("outside function: ",a);
+
+
+
+ function one(){
+    const username = "harsh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username)
+    }
+   // console.log(website)  ->  error
+
+    two()
+ }
+ one()
+
+
+
+ if(true){
+    const username = "harsh"
+    if(username === "harsh"){
+        const website = "youtube"
+        console.log(username + website);
+    }
+ }
+// console.log(username)  ->  outside scope 
