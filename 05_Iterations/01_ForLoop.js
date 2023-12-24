73334da6000b5af9ba373d3d46547ebd4058f373
Harsh-150 <153707537+Harsh-150@users.noreply.github.com>
2023-12-24 09:00:01 +0000

    loops

---
+++ 05_Iterations/01_ForLoop.js
@@ -0,0 +1,9 @@
+for(let i=0; i<10; i++){
+    console.log(i)
+}
+
+let arr = ["flash", "batman", "superman", "spiderman"]
+console.log(arr.length)
+for(let i=0; i<arr.length; i++){
+    console.log(arr[i]);
+}
---
+++ 05_Iterations/02_WhileLoop.js
@@ -0,0 +1,13 @@
+let i=10
+while(i>0){
+    console.log(i)
+    i--
+}
+
+// Do while loop
+
+let score=19
+do{
+    console.log(score)
+    score++
+}while(score<=10)
