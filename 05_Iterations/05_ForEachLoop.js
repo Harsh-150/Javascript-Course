a0593c9de6012ece064aed05762c1d28fb4e8318
Harsh-150 <153707537+Harsh-150@users.noreply.github.com>
2023-12-24 11:02:10 +0000

    For each loop

---
+++ 05_Iterations/05_ForEachLoop.js
@@ -0,0 +1,42 @@
+// const coding = ["js", "cpp", "go", "py", "c", "java"]
+
+// coding.forEach( function (val) {
+//     console.log(val)
+// } )
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// })
+
+// function printMe(item){
+//     console.log(item);
+// }
+// coding.forEach(printMe)
+
+// coding.forEach( (item, index, arr) => {
+//     console.log(item, index, arr)
+// })
+
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "c++",
+        languageFileName: "cpp"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    }
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.languageName)
+} )
